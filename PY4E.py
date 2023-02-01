4b338fc6f06c8b03e29be3ed1f2dafb2b115cd5c
Ivan <argentinaivan@hotmail.com>
2023-02-01 21:44:26 +0100

    Update PY4E.py

--- PY4E.py
+++ PY4E.py
@@ -94,3 +94,13 @@ while True:
 		print('Invalid input')
 	
 print('Sum:', sum, 'Count:', count, 'Average:', sum / count)
+
+# 05 Strings
+
+# Take the following Python code that stores a string: str = 'X-DSPAM-Confidence:0.8475'
+# Use find and string slicing to extract the portion of the string after the colon character and then use the float function to convert the extracted string into a floating point number.
+str = 'X-DSPAM-Confidence:0.8475'
+colon_position = str.find(':')
+number = float(str[colon_position + 1:])
+print(number)
+print(type(number))
