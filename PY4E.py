ac4e59d16d222bcc44d788dd0635d245aa6db1c3
Ivan <argentinaivan@hotmail.com>
2023-02-20 22:22:03 +0100

    Update PY4E.py

--- PY4E.py
+++ PY4E.py
@@ -289,3 +289,35 @@ print(list)
 
 for k, v in list[:1]:
 	print(v, k)
+
+# This program counts the distribution of the hour of the day for each of the messages. You can pull the hour from the “From” line by finding the time string and then splitting that string into parts using the colon character. Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.
+file = input('Enter a file name: ')
+handle = open(file)
+
+dict = {}
+hours = list()
+
+for line in handle:
+	if line.startswith('From '):
+		words = line.split()
+		time = words[5]
+		hour = time.split(':')[0]
+		hours.append(hour)
+
+print(hours)
+
+for hour in hours:
+	dict[hour] = dict.get(hour, 0) + 1
+
+print(dict)
+
+list = list()
+
+for (k, v) in dict.items():
+	tuple = (k, v)
+	list.append(tuple)
+
+list = sorted(list)
+
+for time in list:
+	print(time[0], time[1])
