c48f53030f1c986661a293623f5bc57327d2a712
Ivan <argentinaivan@hotmail.com>
2023-02-22 18:54:28 +0100

    Update PY4E.py

--- PY4E.py
+++ PY4E.py
@@ -321,3 +321,24 @@ list = sorted(list)
 
 for time in list:
 	print(time[0], time[1])
+
+# Write a program that reads a file and prints the letters in decreasing order of frequency. Your program should convert all the input to lower case and only count the letters a-z. Your program should not count spaces, digits, punctuation, or anything other than the letters a-z. 
+import re
+file = input('Enter a file name: ')
+handle = open(file)
+
+text = handle.read().lower()
+
+lower_letters = list()
+dict = {}
+
+for letter in text:
+	if letter.islower():
+		lower_letters.append(letter)
+	
+print(lower_letters)
+
+for lower_letter in lower_letters:
+	dict[lower_letter] = dict.get(lower_letter, 0) + 1
+
+print(dict)
