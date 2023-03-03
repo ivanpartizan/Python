2bea21a6148ed33613b6ad34c207b3cd4ccdb5e2
Ivan <argentinaivan@hotmail.com>
2023-03-04 00:08:27 +0100

    Update codewars.py

--- codewars.py
+++ codewars.py
@@ -1,1 +1,8 @@
-
+# Trimming a string 7kyu
+def trim(phrase, size):
+	if size <= 3 and len(phrase) != 3:
+		return phrase[:size] + '...'
+	if size >= len(phrase):
+		return phrase
+	elif size < len(phrase):
+		return phrase[:size - 3] + '...' 
