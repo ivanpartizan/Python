9432fdd6d5973914bd62e9fb460b1c244b5eb573
Ivan <argentinaivan@hotmail.com>
2023-03-06 14:58:33 +0100

    Update codewars.py

--- codewars.py
+++ codewars.py
@@ -2,7 +2,7 @@
 def trim(phrase, size):
 	if size <= 3 and len(phrase) != 3:
 		return phrase[:size] + '...'
-	if size >= len(phrase):
+	elif size >= len(phrase):
 		return phrase
 	elif size < len(phrase):
 		return phrase[:size - 3] + '...' 
@@ -10,8 +10,8 @@ def trim(phrase, size):
 # Vowel one 7kyu
 def vowel_one(s):
     new_string = ''
-    for letter in s:
-        if letter == 'a' or letter == 'A' or letter == 'e' or letter == 'E' or letter == 'i' or letter == 'I' or letter == 'o' or letter == 'O' or letter == 'u' or letter == 'U':
+    for letter in s.lower():
+        if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
             new_string += '1'
         else:
             new_string += '0'
