432304347dacf0b55f06811aeee1e7219cae165c
Ivan <argentinaivan@hotmail.com>
2023-03-07 23:36:40 +0100

    Update codewars.py

--- codewars.py
+++ codewars.py
@@ -16,3 +16,12 @@ def vowel_one(s):
         else:
             new_string += '0'
     return new_string
+
+# Band name generator 7kyu
+def band_name_generator(name):
+    if len(name) == 1:
+        return name.upper()
+    if name[0] == name[-1]:
+        return name[0].upper() + name[1:-1] + name
+    else:
+        return 'The ' + name[0].upper() + name[1:]
