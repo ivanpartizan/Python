a842e63cff29f32528a76a5ebf74ade7c224d029
Ivan <argentinaivan@hotmail.com>
2023-03-30 23:51:04 +0200

    Update ZTM.py

--- ZTM.py
+++ ZTM.py
@@ -42,3 +42,30 @@ def highest_even(li):
 			if number > highest:
 				highest = number
 	return highest
+
+# Exercise: Cats Everywhere
+# Given the below class:
+class Cat:
+    species = 'mammal'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('Whiskers', 5)
+cat2 = Cat('Smudge', 3)
+cat3 = Cat('Fluffy', 7)
+
+
+# 2 Create a function that finds the oldest cat
+def oldest(*args):
+  oldest = 0
+  for age in args:   
+    if age > oldest:
+      oldest = age
+  return oldest
+  
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f'The oldest cat is {oldest(cat1.age, cat2.age, cat3.age)} years old.')
