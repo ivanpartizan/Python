42f66f2bb559551606f8a2982616bead17b04d13
Ivan <argentinaivan@hotmail.com>
2023-04-01 22:18:39 +0200

    Update ZTM.py

--- ZTM.py
+++ ZTM.py
@@ -69,3 +69,30 @@ def oldest(*args):
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
 print(f'The oldest cat is {oldest(cat1.age, cat2.age, cat3.age)} years old.')
+
+# Exercises: map, filter, zip, reduce
+from functools import reduce
+
+# 1 Capitalize all of the pet names and print the list
+def capitalize(item):
+	return item.capitalize()
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
+print(list(map(capitalize, my_pets)))
+
+# 2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5, 4, 3, 2, 1]
+my_numbers.sort()
+print(list(zip(my_strings, my_numbers)))
+
+# 3 Filter the scores that pass over 50%
+def over50(score):
+	return score > 50
+scores = [73, 20, 65, 19, 76, 100, 88]
+print(list(filter(over50, scores)))
+
+# 4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+joined = scores + my_numbers
+def accumulator(acc, item):
+	return acc + item
+print(reduce(accumulator, joined))
